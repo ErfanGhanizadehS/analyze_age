1fdec1c555303dcfaceced82e0339df1d3635520
ErfanGhanizadeh <eghanizadeh4@gmail.com>
2025-09-08 13:57:24 -0700

    Add files via upload

--- analyze_age.py
+++ analyze_age.py
@@ -1,10 +1,12 @@
 age = int(input("please enter the age:"))
 
+if age<1:
+    print("invaild number")
 if 1<=age<=10:
     print("baby")
 if 10<age<=20:
     print("teen")
 if 20<age<=30:
     print("young")
-if 30<=age:
+if 30<age:
     print("old")
