e7105fceb323daffa2f09f95949b62f7862056db
ErfanGhanizadeh <eghanizadeh4@gmail.com>
2025-09-09 12:47:55 -0700

    Update analyze_age.py

--- analyze_age.py
+++ analyze_age.py
@@ -1,12 +1,12 @@
 age = int(input("please enter the age:"))
 
 if age<1:
-    print("invaild number")
+    print("invalid number")
 if 1<=age<=10:
     print("baby")
 if 10<age<=20:
     print("teen")
 if 20<age<=30:
     print("young")
-if 30<age:
+else:
     print("old")
